238a7f60d30bf7f511ca2904cec06cdbb5201f5a
x6doooo <x6doooo@gmail.com>
2014-05-08 11:25:41 +0800

    update

--- src/sutil/osx/process_info.h
+++ src/sutil/osx/process_info.h
@@ -1,17 +1,25 @@
-#include <vector>
-#include <string>
-#include <iostream>
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>  // for INT_MAX
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
 #include <sys/sysctl.h>
 #include <libproc.h>
+
+#include <vector>
+#include <string>
+#include "../sutil_common.h"
 
 #ifndef _PROCESS_INFO_H_
 #define _PROCESS_INFO_H_
 
-int sutil_pid_exists(long pid);
-int sutil_get_kinfo_proc(pid_t pid, struct kinfo_proc *kp);
-int sutil_get_proc_list(struct kinfo_proc **procList, size_t &procCount);
+bool sutil_pid_exists(const int32_t &pid);
+int sutil_get_kinfo_proc(const pid_t &pid, struct kinfo_proc *kp);
+int sutil_get_proc_list(struct kinfo_proc **procList, size_t *procCount);
 int sutil_get_argmax(void);
-int sutil_proc_pidinfo(long pid, int flavor, void *pti, int size);
-std::vector<std::string> sutil_get_arg_list(long pid);
+int sutil_proc_pidinfo(const int32_t &pid, const int &flavor, void *pti, const int &size);
+std::vector<std::string> sutil_get_arg_list(const int32_t &pid);
 
 #endif
