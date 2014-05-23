79d998ba8a63259c95c84a53c378e23c9babead2
x6doooo <x6doooo@gmail.com>
2014-05-23 11:06:30 +0800

    update sutil_linux

--- src/sutil/sutil_linux.cpp
+++ src/sutil/sutil_linux.cpp
@@ -34,7 +34,7 @@ ioprio_set(int which, int who, int ioprio)
 #define IOPRIO_PRIO_VALUE(class, data) (((class) << IOPRIO_CLASS_SHIFT) | data)
 
 /*
- * Return a (ioclass, iodata) Python tuple representing process I/O priority.
+ * Return {ioclass, iodata} representing process I/O priority.
  */
 int
 sutil_proc_ioprio_get(const int32_t &pid, int* &proc_ioprio)
@@ -42,7 +42,6 @@ sutil_proc_ioprio_get(const int32_t &pid, int* &proc_ioprio)
     int ioprio, ioclass, iodata;
     ioprio = ioprio_get(IOPRIO_WHO_PROCESS, pid);
     if (ioprio == -1) {
-        //PyErr_SetFromErrno(PyExc_OSError);
         return -1;
     }
     ioclass = IOPRIO_PRIO_CLASS(ioprio);
@@ -63,7 +62,6 @@ sutil_proc_ioprio_set(const int32_t &pid, const int &ioclass, const int &iodata)
     int ioprio = IOPRIO_PRIO_VALUE(ioclass, iodata);
     int retval = ioprio_set(IOPRIO_WHO_PROCESS, pid, ioprio);
     if (retval == -1) {
-        //return PyErr_SetFromErrno(PyExc_OSError);
         return -1;
     }
     return 0;
@@ -95,7 +93,6 @@ sutil_linux_prlimit(const pid_t &pid,
         ret = prlimit(pid, resource, NULL, &old);
         if (ret == -1)
             return -1;
-            //return PyErr_SetFromErrno(PyExc_OSError);
 #if defined(SUTIL_HAVE_LONG_LONG)
         if (sizeof(old.rlim_cur) > sizeof(long)) {
             result[0] = old.rlim_cur;
@@ -106,7 +103,6 @@ sutil_linux_prlimit(const pid_t &pid,
         result[0] = old.rlim_cur;
         result[1] = old.rlim_max;
         return 0;
-        //return Py_BuildValue("ll", (long)old.rlim_cur, (long)old.rlim_max);
     }
 
     // set
@@ -115,21 +111,20 @@ sutil_linux_prlimit(const pid_t &pid,
         anew.rlim_cur = *soft;
         if (anew.rlim_cur == (rlim_t) - 1)
             return 0;
-        anew.rlim_max = *hard; //PyLong_AsLongLong(hard);
+        anew.rlim_max = *hard;
         if (anew.rlim_max == (rlim_t) - 1)
             return 0;
 #else
-        anew.rlim_cur = *soft; //PyLong_AsLong(soft);
+        anew.rlim_cur = *soft;
         if (anew.rlim_cur == (rlim_t) - 1)
             return 0;
-        anew.rlim_max = *hard;//PyLong_AsLong(hard);
+        anew.rlim_max = *hard;
         if (anew.rlim_max == (rlim_t) - 1)
             return 0;
 #endif
         newp = &anew;
         ret = prlimit(pid, resource, newp, &old);
         if (ret == -1)
-            //return PyErr_SetFromErrno(PyExc_OSError);
             return -1;
         return 0;
     }
@@ -153,16 +148,11 @@ sutil_pid_exists(const int32_t &pid)
         return true;
     }
 
-    // otherwise return 0 for PID not found
     return false;
 }
 
 
 
-/*
- * return disk mounted partitions as a list of tuples including device,
- * mount point and filesystem type
- */
 int
 sutil_disk_partitions(vector<vector<string>> &devlist)
 {
@@ -174,13 +164,11 @@ sutil_disk_partitions(vector<vector<string>> &devlist)
     // mounted constant comes from mntent.h and it's == '/etc/mtab'
     file = setmntent(MOUNTED, "r");
     if ((file == 0) || (file == NULL)) {
-        //pyerr_setfromerrnowithfilename(pyexc_oserror, mounted);
         goto error;
     }
 
     while ((entry = getmntent(file))) {
         if (entry == NULL) {
-            //pyerr_format(pyexc_runtimeerror, "getmntent() failed");
             goto error;
         }
         tuple.clear();
@@ -208,13 +196,9 @@ sutil_linux_sysinfo(uint64_t* &info)
 {
     struct sysinfo ifo;
     if (sysinfo(&ifo) != 0) {
-        //return PyErr_SetFromErrno(PyExc_OSError);
         return -1;
     }
     // note: boot time might also be determined from here
-    //return Py_BuildValue(
-    //    "(KKKKKK)",
-    //
     info[0] = (uint64_t)ifo.totalram  * ifo.mem_unit;   // total
     info[1] = (uint64_t)ifo.freeram   * ifo.mem_unit;   // free
     info[2] = (uint64_t)ifo.bufferram * ifo.mem_unit;   // buffer
@@ -225,7 +209,7 @@ sutil_linux_sysinfo(uint64_t* &info)
 }
 
 /*
- * Return process CPU affinity as a Python long (the bitmask)
+ * Return process CPU affinity as a long (the bitmask)
  */
 int
 sutil_proc_cpu_affinity_get(const int32_t pid, uint32_t &mask)
@@ -233,10 +217,8 @@ sutil_proc_cpu_affinity_get(const int32_t pid, uint32_t &mask)
     unsigned int len = sizeof(mask);
 
     if (sched_getaffinity(pid, len, (cpu_set_t *)&mask) < 0) {
-        //return PyErr_SetFromErrno(PyExc_OSError);
         return -1;
     }
-    //return Py_BuildValue("l", mask);
     return 0;
 }
 
@@ -250,22 +232,8 @@ sutil_proc_cpu_affinity_set(const int32_t &pid, vector<int32_t> &cpu_set_list)
     size_t len;
     int i;//, seq_len;
 
-    //if (!PySequence_Check(py_cpu_set)) {
-        // does not work on Python 2.4
-        // PyErr_Format(PyExc_TypeError, "sequence argument expected, got %s",
-        //              Py_TYPE(py_cpu_set)->tp_name);
-        //PyErr_Format(PyExc_TypeError, "sequence argument expected");
-        //goto error;
-    //}
-
-    //py_cpu_seq = PySequence_Fast(py_cpu_set, "expected a sequence or integer");
-    //if (!py_cpu_seq) {
-    //    goto error;
-    //}
-    //seq_len = PySequence_Fast_GET_SIZE(py_cpu_seq);
     CPU_ZERO(&cpu_set);
     for (i = 0; i < cpu_set_list.size(); i++) {
-        //PyObject *item = PySequence_Fast_GET_ITEM(py_cpu_seq, i);
         int32_t value = cpu_set_list[i];
         if (value == -1) {
             goto error;
@@ -275,7 +243,6 @@ sutil_proc_cpu_affinity_set(const int32_t &pid, vector<int32_t> &cpu_set_list)
 
     len = sizeof(cpu_set);
     if (sched_setaffinity(pid, len, &cpu_set)) {
-        //PyErr_SetFromErrno(PyExc_OSError);
         goto error;
     }
 
@@ -286,20 +253,15 @@ error:
 }
 
 /*
- * Return currently connected users as a list of tuples.
+ * Return currently connected users.
  */
 int
 sutil_users(vector<sutil_user_info> &user_list)
 {
-    //PyObject *ret_list = PyList_New(0);
-    //PyObject *tuple = NULL;
-    //PyObject *user_proc = NULL;
     bool user_proc;
     sutil_user_info a_user;
     struct utmp *ut;
 
-    //if (ret_list == NULL)
-    //    return NULL;
     setutent();
     while (NULL != (ut = getutent())) {
         if (ut->ut_type == USER_PROCESS)
@@ -307,8 +269,6 @@ sutil_users(vector<sutil_user_info> &user_list)
         else
             user_proc = false;
 
-        //tuple = Py_BuildValue(
-        //    "(sssfO)",
         a_user.username = ut->ut_user;              // username
         a_user.tty = ut->ut_line;              // tty
         a_user.host = ut->ut_host;              // hostname
